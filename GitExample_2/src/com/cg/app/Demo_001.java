4bb781162f0fc9b9d00a159a7eb0d7447c924af9
Kishor Kumar <kishorgn@gmail.com>
2021-03-10 14:26:55 +0530

    Project 2 to demonstrate Git & GitHub

---
+++ GitExample_2/src/com/cg/app/Demo_001.java
@@ -0,0 +1,14 @@
+package com.cg.app;
+
+import com.cg.beans.Student;
+
+public class Demo_001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student(101, "kishor", "kishorgn@gmail.com");
+		System.out.println("s1 : "+s1);
+
+	}
+
+}
