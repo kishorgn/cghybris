d2847e5d0d4c50bc8ebd53cd26f552bb5fbb415d
Kishor Kumar <kishorgn@gmail.com>
2021-03-10 14:19:06 +0530

    Commit - 2. Added second line in both of the programs.

--- GitExample/src/com/cg/app/Demo_01.java
+++ GitExample/src/com/cg/app/Demo_01.java
@@ -5,6 +5,7 @@ public class Demo_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Java Programming");
+		System.out.println("Line 2 added to the program - 1");
 
 	}
 
--- GitExample/src/com/cg/app/Demo_02.java
+++ GitExample/src/com/cg/app/Demo_02.java
@@ -5,6 +5,7 @@ public class Demo_02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Program - 2");
+		System.out.println("Added second line in prog-2");
 
 	}
 
