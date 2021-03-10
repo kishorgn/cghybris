4d9d520aa859f9851631e57fc5998891848e9b29
Kishor Kumar <kishorgn@gmail.com>
2021-03-10 14:15:49 +0530

    Commit - 1, Created 2 files, with 1 line in each.

---
+++ GitExample/src/com/cg/app/Demo_01.java
@@ -0,0 +1,11 @@
+package com.cg.app;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java Programming");
+
+	}
+
+}
---
+++ GitExample/src/com/cg/app/Demo_02.java
@@ -0,0 +1,11 @@
+package com.cg.app;
+
+public class Demo_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Program - 2");
+
+	}
+
+}
