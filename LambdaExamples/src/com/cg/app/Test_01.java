07227bcedf1cb9fbd3ae5bb185382186f9b3bddd
Kishor Kumar <kishorgn@gmail.com>
2021-03-19 13:51:01 +0530

    After adding Test_01 with single line

---
+++ LambdaExamples/src/com/cg/app/Test_01.java
@@ -0,0 +1,11 @@
+package com.cg.app;
+
+public class Test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Simple Program - first line");
+
+	}
+
+}
